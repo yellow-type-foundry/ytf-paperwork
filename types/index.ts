5ac96adc9ca0a63061e6292c87f628414667cf4d
Bao Lam <baolam@gotyme.com>
2025-05-23 15:16:56 +0700

    Add: Type definitions and update ESLint configuration

---
+++ types/index.ts
@@ -0,0 +1,73 @@
+export interface FormItem {
+  typefaceFamily: string
+  typefaceVariant: string
+  typeface: string
+  licenseType: string
+  durationType: string
+  durationYears: number
+  languageCut: string
+  fileFormats: string[]
+  basePrice: number
+  amount: number
+}
+
+export interface FormData {
+  clientName: string
+  clientEmail: string
+  clientAddress: string
+  quotationNumber: string
+  quotationDate: string
+  businessSize: string
+  nonProfitDiscount: boolean
+  customDiscountPercent: number
+  items: FormItem[]
+  subtotal: number
+  tax: number
+  total: number
+}
+
+export interface BusinessSize {
+  id: string
+  name: string
+  description: string
+  multiplier: number | null
+  contactRequired: boolean
+}
+
+export interface Font {
+  family: string
+  variant: string
+  value: string
+  label: string
+}
+
+export interface PDFDocumentProps {
+  data: {
+    quotationNumber: string
+    quotationDate: string
+    clientName: string
+    clientEmail: string
+    clientAddress?: string
+    businessSize?: BusinessSize
+    items: Array<{
+      typeface: string
+      licenseType: string
+      duration: string
+      languageCut: string
+      fileFormats: string
+      amount: number
+    }>
+    subtotal: number
+    total: number
+  }
+}
+
+export interface PDFRendererProps {
+  documentFactory: () => Promise<Blob>
+  fileName: string
+}
+
+export interface FontSelectorProps {
+  onSelectionChange: (fonts: Font[]) => void
+  selectedFonts?: Font[]
+} 
